30bb884bdfacad02a0453f4612927380927afd43
Sush16sn <49299395+Sush16sn@users.noreply.github.com>
2022-07-07 13:55:58 +1000

    Update base.java

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -4,6 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.InputStream;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
